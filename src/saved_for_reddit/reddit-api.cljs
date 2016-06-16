387d33e7241649b22679ff45bfdd6d1cc9a49939
Praveen Vikram <praveen@kiosa.org>
2016-06-16 13:44:38 +0530

    added gen-auth-url

--- src/saved_for_reddit/reddit-api.cljs
+++ src/saved_for_reddit/reddit-api.cljs
@@ -1,11 +1,22 @@
 (ns saved-for-reddit.reddit-api
   (:require [reagent.core :as r]
             [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]])
+            [cljs.core.async :refer [<!]]
+            [cemerick.url :as url])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def client-id "ZZ370hqcmUVsRQ")
 (def redirect-uri "http://127.0.0.1:3449/")
+
+(defn gen-auth-url []
+  (let [cem-url (url/url "https://www.reddit.com/api/v1/authoriz"
+                         :query  {:client_id client-id
+                                  :response_type "code"
+                                  :state "abcdef"
+                                  :redirect_uri redirect-uri
+                                  :duration "temporary"
+                                  :scope "save"} )]
+    (str cem-url)))
 
 (defn make-remote-call [endpoint]
   (go (let [response (<! (http/get endpoint {:with-credentials? false}))]
